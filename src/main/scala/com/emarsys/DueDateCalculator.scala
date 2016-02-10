ddf9a17513dd162255efcab717429249d465205d
u95425 <Pentek13>
2016-02-10 14:03:04 +0100

    One more new solution added using streams (previous method is also kept)

--- src/main/scala/com/emarsys/DueDateCalculator.scala
+++ src/main/scala/com/emarsys/DueDateCalculator.scala
@@ -3,11 +3,21 @@ package com.emarsys
 import com.emarsys.utils.DueDateUtils
 import org.joda.time.{Duration, DateTime, Interval}
 
+import scala.collection.immutable.Stream.#::
+
 /**
   * Created by andra on 2016. 02. 09..
   */
 object DueDateCalculator {
 
+
+  /**
+    * First solution. method is called recursively, and reduce the turnaround time until we can add it to current time before end of workday
+    *
+    * @param currentDate
+    * @param remainingTurnAround
+    * @return
+    */
   def getToDueDAte(currentDate: DateTime, remainingTurnAround: Duration): DateTime = (currentDate, DueDateUtils.remainingWorkhours(currentDate)) match {
     case (cd, rw) if remainingTurnAround.isShorterThan(rw) =>
       currentDate.plus(remainingTurnAround)
@@ -15,6 +25,28 @@ object DueDateCalculator {
       getToDueDAte(currentDate.plus(DueDateUtils.shift(cd)).plus(rw), remainingTurnAround.minus(rw))
   }
 
+  /**
+    * Second solution using a stream, the stream generates hourly the moments initiated with reporting date as head
+    *
+    * @param start
+    * @return
+    */
+  def workHours(start: DateTime): Stream[DateTime] = start #:: {
+    (start, DueDateUtils.remainingWorkhours(start)) match {
+      case (d, rw) if rw.isLongerThan(Duration.standardHours(1)) =>
+        workHours(d.plusHours(1))
+      case _ =>
+        workHours(start.plus(DueDateUtils.shift(start)).plusHours(1))
+    }
+  }
+
+
+  /**
+    * The requested method shoudl be implemented by solving the coding challenge of Emarsys
+    * @param reportDate The exact date when the task has been reported i a reporting system
+    * @param turnAround The turnaround time is given in working hours
+    * @return
+    */
   def calculateDueDate(reportDate: DateTime, turnAround: Duration): DateTime = reportDate match {
     case d if d.getDayOfWeek() > 5 =>
       throw new IllegalArgumentException("reportDate should be on weekdays between 9am to 5pm.")
@@ -23,7 +55,16 @@ object DueDateCalculator {
       throw new IllegalArgumentException("reportDate should be on weekdays between 9am to 5pm.")
 
     case validReportDate => {
-      getToDueDAte(reportDate, turnAround)
+      /**
+        * Frist solution
+        */
+      //getToDueDAte(reportDate, turnAround)
+
+      /**
+        * Second solution - we initiate the of the stream with the reportDate and get the nth element of the stream (n = turnAroundduration in hours)
+        * I stick to use the turnaround time as a duration to make room for further modifications
+        */
+      workHours(reportDate)(turnAround.getStandardHours.toInt)
     }
 
   }
